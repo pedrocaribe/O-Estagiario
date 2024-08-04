eb3c8d87c5ffca11802fd4a13527ac7358e33bff
Pedro Caribe <pho.caribe@gmail.com>
2024-08-03 23:24:03 -0300

    Commenting in English and change of variable names

--- index.js
+++ index.js
@@ -7,80 +7,34 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 
-// Inicializa o cliente WhatsApp com autenticação local
+// Initialize WhatsApp client with local authentication
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: `${CLIENT_ID}` }),
     puppeteer: { headless: true }
 });
 
-// Exibe o QR code para login no WhatsApp
+// Show QR Code for WhatsApp login if needed
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
     console.log('Escaneie o QR Code acima para logar no WhatsApp.');
 });
 
-// Quando o cliente WhatsApp estiver pronto
+// When WhatsApp client is ready
 client.on('ready', async () => {
     console.log('Estagiário está pronto para trabalhar!');
     
-    // Inicia a verificação periódica de novos materiais a cada 60 segundos
+    // Begin new courseware verification every 60 seconds 
     setInterval(() => {
         checkForNewMaterials(client);
-    }, 60000); // 60000 milissegundos = 60 segundos
+    }, 60000);
 });
 
-// Escuta por mensagens recebidas
+// Listen for ALL received messages 
 client.on('message_create', async (message) => {
     if (message.type == 'ptt'){
         transcreverAudio(client, message);
     }
 });
 
+// Initialize WhatsApp client
 client.initialize();
-
-
-
-// const { Client, LocalAuth } = require('whatsapp-web.js');
-// const { transcreverAudio } = require('./comandos');
-// const { checkForNewMaterials } = require('./universidade');
-// const qrcode = require('qrcode-terminal');
-
-// require('dotenv').config();
-
-// const CLIENT_ID = process.env.CLIENT_ID;
-
-// // Inicializa o cliente WhatsApp com autenticação local
-// const client = new Client({
-//     authStrategy: new LocalAuth({ clientId: CLIENT_ID }),
-//     puppeteer: { headless: true },
-//     // Garante que mensagens próprias também sejam capturadas
-//     takeoverOnConflict: true,
-//     takeoverTimeoutMs: 0,
-// });
-
-// // Exibe o QR code para login no WhatsApp
-// client.on('qr', qr => {
-//     qrcode.generate(qr, { small: true });
-//     console.log('Escaneie o QR Code acima para logar no WhatsApp.');
-// });
-
-// // Quando o cliente WhatsApp estiver pronto
-// client.on('ready', () => {
-//     console.log('Estagiário está pronto para trabalhar!');
-// });
-
-// // Escuta por mensagens recebidas
-// client.on('message', async message => {
-//     console.log(`Mensagem recebida de ${message.from}: ${message.body}`);
-//     if (message.fromMe) {
-//         console.log('Esta mensagem foi enviada por você.');
-//     } else {
-//         console.log('Esta mensagem foi enviada por outra pessoa.');
-//     }
-// });
-
-// client.on('message_create', async (msg) => {
-//     console.log(`Mensagem recebida de ${msg.from}: ${msg.body}}`);
-// });
-
-// client.initialize();
