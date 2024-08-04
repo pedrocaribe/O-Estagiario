0dc1b8ecbe010a39ce3f1a600e030ce330abcc32
Pedro Caribe <pho.caribe@gmail.com>
2024-08-03 23:18:13 -0300

    Commenting in English and change of variable names

--- universidade.js
+++ universidade.js
@@ -13,23 +13,23 @@ const USER_RA = process.env.USER_RA;
 const USER_PASS = process.env.USER_PASS;
 const GOOGLE_DRIVE_FOLDER = process.env.GOOGLE_DRIVE_FOLDER;
 
-// Função para enviar uma mensagem no WhatsApp
+// Function to send WhatsApp message
 async function sendWhatsAppMessage(client, subjectName, fileName, filePath) {
-    const chatId = PRIVATE_CHAT_ID; // Substitua pelo ID do grupo desejado
+    const chatId = PRIVATE_CHAT_ID;
     const message = `Novo arquivo postado para ${subjectName}: ${fileName}`;
     await client.sendMessage(chatId, message);
 
-    // Envia o arquivo como documento
+    // Send file as a Document
     const media = MessageMedia.fromFilePath(filePath);
     await client.sendMessage(chatId, media, { sendMediaAsDocument: true });
 }
 
-// Função para verificar se há novos materiais
+// Function to check if there are new files in University portal
 async function checkForNewMaterials(client) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    // Configura uma instância do axios para ignorar certificados SSL inválidos
+    // Configure axios instance to ignore invalid SSL certificates
     const axiosInstance = axios.create({
         httpsAgent: new https.Agent({
             rejectUnauthorized: false
@@ -37,10 +37,10 @@ async function checkForNewMaterials(client) {
     });
 
     try {
-        // Acessa a página de login
+        // Access University login page
         await page.goto(UNISANTA_URL, { waitUntil: 'networkidle2' });
 
-        // Desabilita animações na página
+        // Disable all animations in order to process information faster
         await page.evaluate(() => {
             const style = document.createElement('style');
             style.type = 'text/css';
@@ -53,56 +53,60 @@ async function checkForNewMaterials(client) {
             document.head.appendChild(style);
         });
 
-        // Preenche o formulário de login
+        // Fill Login form
         await page.type('#User', USER_RA); // Insira seu RA
         await page.type('#Pass', USER_PASS); // Insira sua senha
 
-        // Envia o formulário
+        // Send Login form
         await page.click('input[type="submit"]');
         await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 5000 });
 
-        // Interage com o botão "Confirmar"
+        // Click on "Confirmar" button
         await page.waitForSelector('#btnConfirmar', { timeout: 5000 });
-        await new Promise(resolve => setTimeout(resolve, 300)); // Substituindo waitForTimeout
+        await new Promise(resolve => setTimeout(resolve, 300)); // Timeout needed before clicking
         await page.click('#btnConfirmar');
 
-        // Espera até que o link de "Material Didático" esteja visível e clica nele
+        // Wait until "Material Didatico" button is available and click on it
         await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 5000 });
         await page.waitForSelector('a[href="/Academico/MaterialDidatico"]', { timeout: 5000 });
-        await new Promise(resolve => setTimeout(resolve, 500)); // Substituindo waitForTimeout
+        await new Promise(resolve => setTimeout(resolve, 500)); // Timeout needed before clicking
         await page.click('a[href="/Academico/MaterialDidatico"]');
 
-        // Pega todas as disciplinas
-        const disciplinas = await page.$$('.disciplinasMaterialDidatico');
-        console.log(`Encontradas ${disciplinas.length} disciplinas.`);
+        // List all courses
+        const courses = await page.$$('.disciplinasMaterialDidatico');
+        console.log(`Encontradas ${courses.length} disciplinas.`);
 
-        for (let i = 0; i < disciplinas.length; i++) {
-            const disciplinas = await page.$$('.disciplinasMaterialDidatico');
-            const disciplina = disciplinas[i];
-            const disciplinaNome = await page.evaluate(el => el.innerText.trim().toUpperCase(), disciplina);
+        // Iterate through all courses and check for new files posted by Professors
+        for (let i = 0; i < courses.length; i++) {
+            const courses = await page.$$('.disciplinasMaterialDidatico'); // Needed due to dynamic changing of elements
+            const course = courses[i];
+            const courseName = await page.evaluate(el => el.innerText.trim().toUpperCase(), course);
 
-            await new Promise(resolve => setTimeout(resolve, 500)); // Substituindo waitForTimeout
-            console.log(`Acessando disciplina ${i + 1}: ${disciplinaNome}`);
-            await disciplina.click();
+            await new Promise(resolve => setTimeout(resolve, 500)); // Timeout needed before clicking
+            console.log(`Acessando disciplina ${i + 1}: ${courseName}`);
+            await course.click();
             await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 5000 });
 
-            // Pega todos os links de materiais para download
-            const materiais = await page.$$eval("td strong > a[href*='Download']", links => links.map(link => ({
+            // Fetch all files download links
+            const courseware = await page.$$eval("td strong > a[href*='Download']", links => links.map(link => ({
                 url: link.href,
                 fileName: link.href.split('NomeDoArquivoOriginal=')[1].split("&")[0],
                 fileExtension: link.href.split('Extensao=')[1].split("&")[0],
             })));
 
-            console.log(`Encontrados ${materiais.length} materiais para Download.`);
+            console.log(`Encontrados ${courseware.length} materiais para Download.`);
 
-            for (let material of materiais) {
-                const downloadDir = path.resolve(GOOGLE_DRIVE_FOLDER, disciplinaNome);
+            // Iterate through all files and checks for duplicate, saves to Google Drive and Send as WhatsApp message
+            for (let material of courseware) {
+                const downloadDir = path.resolve(GOOGLE_DRIVE_FOLDER, courseName);
                 const filePath = path.resolve(downloadDir, `${material.fileName}.${material.fileExtension}`);
 
+                // Checks for folder path existence
                 if (!fs.existsSync(downloadDir)) {
                     fs.mkdirSync(downloadDir, { recursive: true });
                 }
 
+                // Checks for duplicate files
                 if (!fs.existsSync(filePath)) {
                     try {
                         console.log(`Baixando ${material.fileName}.${material.fileExtension}`);
@@ -110,8 +114,8 @@ async function checkForNewMaterials(client) {
                         fs.writeFileSync(filePath, response.data);
                         console.log(`Download de ${material.fileName}.${material.fileExtension} concluído.`);
 
-                        // Envia a mensagem no WhatsApp após o download
-                        await sendWhatsAppMessage(client, disciplinaNome, `${material.fileName}.${material.fileExtension}`, filePath);
+                        // Send WhatsApp message after Download
+                        await sendWhatsAppMessage(client, courseName, `${material.fileName}.${material.fileExtension}`, filePath);
                     } catch (error) {
                         console.log(`Erro ao baixar ${material.fileName}.${material.fileExtension}:`, error);
                     }
@@ -120,7 +124,7 @@ async function checkForNewMaterials(client) {
                 }
             }
 
-            // Voltar para a lista de disciplinas
+            // Back to course list to iterate through next course.
             await page.goBack({ waitUntil: 'networkidle2' });
         }
 
