8692c0d4a8d40a149ba1d78e3b7a07d7f2097040
Pedro Caribe <pho.caribe@gmail.com>
2024-08-04 03:29:22 -0300

    changed approach to register commands

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 
+
+// Implementation of decorators for commands
+const commands = {};
+
+function registerCommand(command, callback) {
+    commands[command] = callback;
+}
+
+
 // Initialize WhatsApp client with local authentication
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: `${CLIENT_ID}` }),
@@ -22,18 +31,41 @@ client.on('qr', qr => {
 // When WhatsApp client is ready
 client.on('ready', async () => {
     console.log('Estagiário está pronto para trabalhar!');
-    
+
     // Begin new courseware verification every 60 seconds 
     setInterval(() => {
         checkForNewMaterials(client);
     }, 60000);
 });
 
+
 // Listen for ALL received messages 
 client.on('message_create', async (message) => {
-    if (message.type == 'ptt'){
-        transcribeAudio(client, message);
+    const args = message.body.trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (commands[commandName]) {
+        try {
+            await commands[commandName](message, args);
+        } catch (error) {
+            console.error(`Erro ao executar comando ${commandName}:`, error);
+        }
     }
+});
+
+// Register the '!transcrever' command manually
+registerCommand('!transcrever', async (message, args) => {
+    if (!message.hasQuotedMsg) {
+        return await message.reply("Você tem que responder ao *áudio* com o comando _*!transcrever*_");
+    }
+
+    const quotedMessage = await message.getQuotedMessage();
+
+    if (['ptt', 'audio'].includes(quotedMessage.type)) {
+        return transcribeAudio(client, quotedMessage);
+    } 
+
+    return await message.reply(`Essa não é uma mensagem de *áudio*.`);
 });
 
 // Initialize WhatsApp client
